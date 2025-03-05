4a52e80fa5f2c560b82429c6dcd5f6ef0c279d59
cschiro <cschiro90@gmail.com>
2025-03-05 15:29:16 -0800

    update

--- server/src/server.js
+++ server/src/server.js
@@ -18,22 +18,22 @@ app.use('/user',router)
 
 //Boilerplate to check if the code is working correctly---------------------------
 app.get('/', (req, res) => {
-  res.status(200).json('Hello from the backend');
+  return res.status(200).json('Hello from the backend');
 });
 //Boilerplate to send a 404 error if page doesn't exit---------------------------
 app.use((req, res) => {
-  res.status(404).json('This is a 404 error');
+  return res.status(404).json('This is a 404 error');
 });
 
 //Boilerplate to send a 500 error if system error--------------------------------
 app.use((err, req, res, next) => {
-  const defaultErr = {
+   const defaultErr = {
     log: 'You are receiving an error from the server',
     status: 500,
     message: { err: 'This is a 500 error message' },
   };
   const errorObj = Object.assign(defaultErr, err);
-  res.status(errorObj.status).send(errorObj.message);
+  return res.status(errorObj.status).send(errorObj.message);
 });
 
 //Boilerplate to send a console.log when the server starts--------------------------------
